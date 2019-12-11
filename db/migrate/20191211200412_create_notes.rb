c6b0e63434f314c67dd574e03bd2549cc46088bd
nasia-sam <nasiasam@gmail.com>
2019-12-11 20:05:25 +0000

    Add notes

---
+++ db/migrate/20191211200412_create_notes.rb
@@ -0,0 +1,12 @@
+class CreateNotes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :notes do |t|
+      t.string :title
+      t.text :content
+      t.references :user, foreign_key: true
+      t.references :course, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
